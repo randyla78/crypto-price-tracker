cb65d3404a14839e2645ad02395370e39370866b
Randy La <randyla78@gmail.com>
2025-03-03 19:21:16 -0500

    Fixed docusaurus bug

--- crypto-tracker-docs/docusaurus.config.js
+++ crypto-tracker-docs/docusaurus.config.js
@@ -49,7 +49,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'project-setup',
             position: 'left',
             label: 'Tutorial',
           },
@@ -69,7 +69,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/project-setup',
               },
             ],
           },
